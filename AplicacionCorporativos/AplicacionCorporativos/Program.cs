b01c53bb65c387a56d6c2cd6c19b702b70223463
Mateo <64622961+MateoCorigliano@users.noreply.github.com>
2021-11-06 21:06:45 -0300

    Apertura de proyecto
    se crea la aplicacion de consola

---
+++ AplicacionCorporativos/AplicacionCorporativos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionCorporativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esto es una prueba para verificar que el los cambios y actualizaciones de los usuarios funcionan
+            //colocar al lado del nombre la palabra OK , guardar y actualizar el proyecto
+            //Mate -> OK
+            //Agus ->
+            //Eli ->
+            //Axel ->
+            //Mauri ->
+        }
+    }
+}
